e0d72a99315b0480e54a109dc5652574573bfec7
Dima Lampsi <dlampsi@gmail.com>
2024-04-30 15:43:21 +0300

    Improving and refactoring tests

--- adc_test.go
+++ adc_test.go
@@ -17,7 +17,7 @@ func Test_New(t *testing.T) {
 	t.Run("Nil", func(t *testing.T) {
 		cl := New(nil)
 		require.NotNil(t, cl)
-		require.NotNil(t, cl.cfg)
+		require.NotNil(t, cl.Config)
 	})
 	t.Run("WithLogger", func(t *testing.T) {
 		tlogger := &testLogger{}
@@ -27,84 +27,111 @@ func Test_New(t *testing.T) {
 	})
 }
 
-func Test_Client_Config(t *testing.T) {
-	t.Run("Default", func(t *testing.T) {
+func Test_Client_Connect(t *testing.T) {
+	t.Run("BadClient", func(t *testing.T) {
 		cl := New(nil)
-		require.NotNil(t, cl.Config())
+		require.Error(t, cl.Connect(), "Connection should fail without any configuration")
+	})
+	t.Run("Bindless", func(t *testing.T) {
+		cl := newMockClient(nil)
+		require.NoError(t, cl.Connect(), "Bindless connection should be successful")
+		require.NoError(t, cl.Disconnect())
+	})
+	t.Run("BindWithErr", func(t *testing.T) {
+		cl := newMockClient(&Config{
+			Bind: &BindAccount{DN: "mrError", Password: "fake"},
+		})
+		require.Error(t, cl.Connect(), "Connection should fail with bad bind credentials")
+	})
+	t.Run("BindWithSuccess", func(t *testing.T) {
+		cl := newMockClient(&Config{
+			Bind: validMockBind,
+		})
+		require.NoError(t, cl.Connect(), "Connection should be successful with valid bind credentials")
+	})
+	t.Run("WithSecureDialOpts", func(t *testing.T) {
+		cl := New(&Config{
+			URL: "ldaps://fake:636",
+		})
+		require.Error(t, cl.Connect())
+	})
+	t.Run("WithSecureDialOpts", func(t *testing.T) {
+		cl := New(&Config{
+			URL:         "ldaps://fake:636",
+			InsecureTLS: true,
+		})
+		require.Error(t, cl.Connect())
 	})
 }
 
-func Test_Client_Connect(t *testing.T) {
-	badCl := New(nil)
-	expErr := badCl.Connect()
-	require.Error(t, expErr)
-
-	cl := New(nil, withMock())
-
-	err := cl.Connect()
-	require.NoError(t, err)
-	require.NoError(t, cl.Disconnect())
-
-	cfg := &Config{
-		Bind: &BindAccount{DN: "fakeone", Password: "fake"},
-	}
-	cl = New(cfg, withMock())
-	err = cl.Connect()
-	require.Error(t, err)
-
-	cfg.Bind.DN = "mrError"
-	cl = New(cfg)
-	err = cl.Connect()
-	require.Error(t, err)
-
-	cfg.Bind.DN = "validUser"
-	cfg.Bind.Password = "badPass"
-	cl = New(cfg, withMock())
-	err = cl.Connect()
-	require.Error(t, err)
-
-	cfg.Bind.DN = "validUser"
-	cfg.Bind.Password = "validPass"
-	cl = New(cfg, withMock())
-	err = cl.Connect()
-	require.NoError(t, err)
+func Test_Client_Disconnect(t *testing.T) {
+	t.Run("WithNilLdap", func(t *testing.T) {
+		cl := newMockClient(&Config{Bind: validMockBind})
+		require.NoError(t, cl.Disconnect())
+	})
+	t.Run("Ok", func(t *testing.T) {
+		cl := newMockClient(&Config{Bind: validMockBind})
+		require.NoError(t, cl.Connect())
+		require.NoError(t, cl.Disconnect())
+	})
 }
 
 func Test_Client_Reconnect(t *testing.T) {
-	cfg := &Config{
-		Bind: validMockBind,
-	}
-	cl := New(cfg, withMock())
-	err := cl.Connect()
-	require.NoError(t, err)
+	t.Run("WithErr", func(t *testing.T) {
+		cl := newMockClient(&Config{Bind: validMockBind})
+		require.NoError(t, cl.Connect())
 
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
+		cl.Config.Bind = &BindAccount{DN: "OU=entryForErr,DC=company,DC=com"}
+		require.Error(t, cl.Reconnect(context.TODO(), 2*time.Second, 2), "Reconnect for a bad user should fail with error")
+	})
+	t.Run("WithReconnectErr", func(t *testing.T) {
+		ctx := context.TODO()
 
-	err = cl.Reconnect(ctx, 2*time.Second, 2)
-	require.NoError(t, err)
+		cl := newMockClient(&Config{Bind: validMockBind})
+		require.NoError(t, cl.Connect())
 
-	cl.cfg.Bind = &BindAccount{DN: mockEntriesData["entryForErr"].DN}
-	err = cl.Reconnect(ctx, 2*time.Second, 2)
-	require.Error(t, err)
+		cl.Config.Bind = reconnectMockBind
 
-	cl.cfg.Bind = reconnectMockBind
-	err = cl.Reconnect(ctx, 0, 1)
-	require.Error(t, err)
-	err = cl.Reconnect(ctx, 30*time.Millisecond, 0)
-	require.Error(t, err)
-	err = cl.Reconnect(ctx, 1*time.Second, 1)
-	require.Error(t, err)
+		require.Error(t, cl.Reconnect(ctx, 0, 1))
+		require.Error(t, cl.Reconnect(ctx, 30*time.Millisecond, 0))
+		require.Error(t, cl.Reconnect(ctx, 1*time.Second, 1))
+	})
+	t.Run("WithContextCancel", func(t *testing.T) {
+		ctx, cancel := context.WithCancel(context.Background())
+		go func() {
+			time.Sleep(10 * time.Millisecond)
+			cancel()
+		}()
 
-	nctx, cancel := context.WithCancel(context.Background())
-	go func() {
-		time.Sleep(10 * time.Millisecond)
-		cancel()
-	}()
-	err = cl.Reconnect(nctx, 5*time.Second, 1)
-	require.Error(t, err)
+		cl := newMockClient(&Config{Bind: validMockBind})
+		require.NoError(t, cl.Connect())
 
-	cl.cfg.Bind = validMockBind
-	err = cl.Reconnect(ctx, 30*time.Millisecond, 1)
-	require.NoError(t, err)
+		cl.Config.Bind = reconnectMockBind
+
+		require.Error(t, cl.Reconnect(ctx, 5*time.Second, 1))
+
+		cl.Config.Bind = validMockBind
+		require.NoError(t, cl.Reconnect(ctx, 30*time.Millisecond, 1))
+	})
+	t.Run("Ok", func(t *testing.T) {
+		ctx := context.TODO()
+
+		cl := newMockClient(&Config{Bind: validMockBind})
+
+		require.NoError(t, cl.Connect())
+		require.NoError(t, cl.Reconnect(ctx, 2*time.Second, 2), "Reconnect should be successful")
+	})
+}
+
+func Test_Client_CheckAuthByDN(t *testing.T) {
+	t.Run("WithCheckErr", func(t *testing.T) {
+		cl := newMockClient(&Config{Bind: validMockBind})
+		require.NoError(t, cl.Connect())
+		require.Error(t, cl.CheckAuthByDN("OU=entryForErr,DC=company,DC=com", "fake"))
+	})
+	t.Run("Ok", func(t *testing.T) {
+		cl := newMockClient(&Config{Bind: validMockBind})
+		require.NoError(t, cl.Connect())
+		require.NoError(t, cl.CheckAuthByDN(validMockBind.DN, validMockBind.Password))
+	})
 }
--- user_test.go
+++ user_test.go
@@ -7,137 +7,173 @@ import (
 )
 
 func Test_User_GetStringAttribute(t *testing.T) {
-	u := &User{
-		Attributes: map[string]interface{}{
-			"one":   "string",
-			"two":   2,
-			"three": []byte("bytedata"),
-		},
-	}
-	require.NotEmpty(t, u.GetStringAttribute("one"))
-	require.Equal(t, "string", u.GetStringAttribute("one"))
-	require.Empty(t, u.GetStringAttribute("two"))
-	require.Empty(t, u.GetStringAttribute("three"))
-	require.Empty(t, u.GetStringAttribute("nonexists"))
+	t.Run("NonExists", func(t *testing.T) {
+		u := &User{
+			Attributes: map[string]interface{}{
+				"one": "string",
+			},
+		}
+		require.Empty(t, u.GetStringAttribute("nonexists"))
+	})
+	t.Run("NonString", func(t *testing.T) {
+		user := &User{
+			Attributes: map[string]interface{}{
+				"two":   2,
+				"three": []byte("bytedata"),
+			},
+		}
+		require.Equal(t, "", user.GetStringAttribute("two"))
+		require.Equal(t, "", user.GetStringAttribute("three"))
+	})
+	t.Run("Ok", func(t *testing.T) {
+		user := &User{
+			Attributes: map[string]interface{}{
+				"one": "value",
+			},
+		}
+		require.Equal(t, "value", user.GetStringAttribute("one"))
+	})
 }
 
 func Test_GetUserArgs_Validate(t *testing.T) {
-	var req GetUserArgs
-	err := req.Validate()
-	require.Error(t, err)
-
-	req = GetUserArgs{}
-	err1 := req.Validate()
-	require.Error(t, err1)
-
-	req = GetUserArgs{Id: "fake"}
-	errOk := req.Validate()
-	require.NoError(t, errOk)
-
-	req = GetUserArgs{Filter: "fake"}
-	errOk = req.Validate()
-	require.NoError(t, errOk)
-
-	req = GetUserArgs{Dn: "fake"}
-	errOk = req.Validate()
-	require.NoError(t, errOk)
+	t.Run("ErrWithNil", func(t *testing.T) {
+		var req GetUserArgs
+		require.Error(t, req.Validate())
+	})
+	t.Run("ErrWithEmpty", func(t *testing.T) {
+		req := GetUserArgs{}
+		require.Error(t, req.Validate())
+	})
+	t.Run("OkWithId", func(t *testing.T) {
+		req := GetUserArgs{Id: "fake"}
+		require.NoError(t, req.Validate())
+	})
+	t.Run("OkWithFilter", func(t *testing.T) {
+		req := GetUserArgs{Filter: "fake"}
+		require.NoError(t, req.Validate())
+	})
+	t.Run("OkWithDn", func(t *testing.T) {
+		req := GetUserArgs{Dn: "fake"}
+		require.NoError(t, req.Validate())
+	})
 }
 
 func Test_Client_GetUser(t *testing.T) {
-	cl := New(&Config{}, withMock())
-	err := cl.Connect()
-	require.NoError(t, err)
+	cl := newMockClient(&Config{})
+	require.NoError(t, cl.Connect())
 
-	var badArgs GetUserArgs
-	_, badReqErr := cl.GetUser(badArgs)
-	require.Error(t, badReqErr)
-
-	args := GetUserArgs{Id: "entryForErr", SkipGroupsSearch: true}
-	_, err = cl.GetUser(args)
-	require.Error(t, err)
-
-	args = GetUserArgs{Id: "userFake", SkipGroupsSearch: true}
-	user, err := cl.GetUser(args)
-	require.NoError(t, err)
-	require.Nil(t, user)
-
-	// Too many entries error
-	user, err = cl.GetUser(GetUserArgs{
-		Id:               "notUniq",
-		SkipGroupsSearch: true,
-		Attributes:       []string{"sAMAccountName"},
+	t.Run("BadArgs", func(t *testing.T) {
+		var badArgs GetUserArgs
+		_, badReqErr := cl.GetUser(badArgs)
+		require.Error(t, badReqErr)
 	})
-	require.Error(t, err)
-	require.Nil(t, user)
+	t.Run("EntryForErr", func(t *testing.T) {
+		args := GetUserArgs{Id: "entryForErr", SkipGroupsSearch: true}
+		u, err := cl.GetUser(args)
+		require.Error(t, err)
+		require.Nil(t, u)
+	})
+	t.Run("NonExistsUser", func(t *testing.T) {
+		args := GetUserArgs{Id: "userFake", SkipGroupsSearch: true}
+		user, err := cl.GetUser(args)
+		require.NoError(t, err)
+		require.Nil(t, user)
+	})
+	t.Run("TooManyEntries", func(t *testing.T) {
+		user, err := cl.GetUser(GetUserArgs{
+			Id:               "notUniq",
+			SkipGroupsSearch: true,
+			Attributes:       []string{"sAMAccountName"},
+		})
+		require.Error(t, err)
+		require.Nil(t, user)
+	})
+	t.Run("ByDn", func(t *testing.T) {
+		dnReq := GetUserArgs{Dn: "OU=user1,DC=company,DC=com", SkipGroupsSearch: true}
+		userByDn, err := cl.GetUser(dnReq)
+		require.NoError(t, err)
+		require.NotNil(t, userByDn)
+		require.Equal(t, dnReq.Dn, userByDn.DN)
+	})
+	t.Run("ByFilter", func(t *testing.T) {
+		filterReq := GetUserArgs{Filter: "customFilterToSearchUser", SkipGroupsSearch: true}
+		userByFilter, err := cl.GetUser(filterReq)
+		require.NoError(t, err)
+		require.NotNil(t, userByFilter)
+		require.Equal(t, userByFilter.Id, "user1")
+	})
+	t.Run("Ok", func(t *testing.T) {
+		args := GetUserArgs{Id: "user1", SkipGroupsSearch: true}
+		user, err := cl.GetUser(args)
+		require.NoError(t, err)
+		require.NotNil(t, user)
+		require.Equal(t, args.Id, user.Id)
+		require.Nil(t, user.Groups)
 
-	dnReq := GetUserArgs{Dn: "OU=user1,DC=company,DC=com", SkipGroupsSearch: true}
-	userByDn, err := cl.GetUser(dnReq)
-	require.NoError(t, err)
-	require.NotNil(t, userByDn)
-	require.Equal(t, dnReq.Dn, userByDn.DN)
+		args.Attributes = []string{"something"}
+		user, err = cl.GetUser(args)
+		require.NoError(t, err)
+		require.NotNil(t, user)
+		require.Equal(t, args.Id, user.Id)
+		require.Nil(t, user.Groups)
 
-	filterReq := GetUserArgs{Filter: "customFilterToSearchUser", SkipGroupsSearch: true}
-	userByFilter, err := cl.GetUser(filterReq)
-	require.NoError(t, err)
-	require.NotNil(t, userByFilter)
-	require.Equal(t, userByFilter.Id, "user1")
-
-	args = GetUserArgs{Id: "user1", SkipGroupsSearch: true}
-	user, err = cl.GetUser(args)
-	require.NoError(t, err)
-	require.NotNil(t, user)
-	require.Equal(t, args.Id, user.Id)
-	require.Nil(t, user.Groups)
-
-	args.Attributes = []string{"something"}
-	user, err = cl.GetUser(args)
-	require.NoError(t, err)
-	require.NotNil(t, user)
-	require.Equal(t, args.Id, user.Id)
-	require.Nil(t, user.Groups)
-
-	args.SkipGroupsSearch = false
-	user, err = cl.GetUser(args)
-	require.NoError(t, err)
-	require.NotNil(t, user)
-	require.Equal(t, args.Id, user.Id)
-	require.NotNil(t, user.Groups)
-	require.Len(t, user.Groups, 1)
+		args.SkipGroupsSearch = false
+		user, err = cl.GetUser(args)
+		require.NoError(t, err)
+		require.NotNil(t, user)
+		require.Equal(t, args.Id, user.Id)
+		require.NotNil(t, user.Groups)
+		require.Len(t, user.Groups, 1)
+	})
 }
 
 func Test_User_IsGroupMember(t *testing.T) {
-	u := &User{}
-	require.Equal(t, false, u.IsGroupMember("group1"))
-
-	u.Groups = []UserGroup{
-		{Id: "group1"},
-		{Id: "group2"},
-	}
-	require.Equal(t, false, u.IsGroupMember("group3"))
-	require.Equal(t, true, u.IsGroupMember("group1"))
-	require.Equal(t, true, u.IsGroupMember("group2"))
+	t.Run("EmptyGroups", func(t *testing.T) {
+		u := &User{}
+		require.Equal(t, false, u.IsGroupMember("group1"))
+	})
+	t.Run("NotAMember", func(t *testing.T) {
+		u := &User{
+			Groups: []UserGroup{{Id: "group1"}},
+		}
+		require.Equal(t, false, u.IsGroupMember("group3"))
+	})
+	t.Run("IsMember", func(t *testing.T) {
+		u := &User{
+			Groups: []UserGroup{{Id: "group1"}, {Id: "group2"}},
+		}
+		require.Equal(t, true, u.IsGroupMember("group1"))
+	})
 }
 
 func Test_User_GroupsDn(t *testing.T) {
-	u := &User{
-		Groups: []UserGroup{},
-	}
-	require.Nil(t, u.GroupsDn())
-
-	newGroup := UserGroup{Id: "someId", DN: "someDn"}
-	u.Groups = append(u.Groups, newGroup)
-	require.NotNil(t, u.GroupsDn())
-	require.Contains(t, u.GroupsDn(), newGroup.DN)
+	t.Run("EmptyGroups", func(t *testing.T) {
+		u := &User{
+			Groups: []UserGroup{},
+		}
+		require.Nil(t, u.GroupsDn())
+	})
+	t.Run("Ok", func(t *testing.T) {
+		u := &User{
+			Groups: []UserGroup{{Id: "someId", DN: "someDn"}},
+		}
+		require.NotNil(t, u.GroupsDn())
+		require.Contains(t, u.GroupsDn(), "someDn")
+	})
 }
 
 func Test_User_GroupsId(t *testing.T) {
-	u := &User{
-		Groups: []UserGroup{},
-	}
-
-	require.Nil(t, u.GroupsId())
-	newGroup := UserGroup{Id: "someId", DN: "someDn"}
-	u.Groups = append(u.Groups, newGroup)
-	require.NotNil(t, u.GroupsId())
-	require.Contains(t, u.GroupsId(), newGroup.Id)
+	t.Run("EmptyGroups", func(t *testing.T) {
+		u := &User{
+			Groups: []UserGroup{},
+		}
+		require.Nil(t, u.GroupsId())
+	})
+	t.Run("Ok", func(t *testing.T) {
+		u := &User{
+			Groups: []UserGroup{{Id: "someId", DN: "someDn"}},
+		}
+		require.NotNil(t, u.GroupsId())
+		require.Contains(t, u.GroupsId(), "someId")
+	})
 }
